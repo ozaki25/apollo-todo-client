1a37a71443a7c40afccfabb5a0def095980a3d1c
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-27 22:42:26 +0900

    add TodoList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import client from './graphql/client';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <h1>Hello</h1>
+      <TodoList />
     </ApolloProvider>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_TODOS } from '../graphql/query';
+
+function TodoList() {
+  const { loading, error, data } = useQuery(GET_TODOS);
+
+  if (loading) return <p>...loading</p>;
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <div>
+      {data.todos.map(todo => (
+        <p key={todo.id}>
+          ID: {todo.id}, Todo: {todo.type}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
